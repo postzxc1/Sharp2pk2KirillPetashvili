83f98fc44ca79023b6c1eba34277319449ec4c03
postzxc1 <kpetasvili@gmail.com>
2023-05-09 14:59:36 +0500

    Добавлено практическое задание pz_./pz_22

---
+++ pz_22/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace PZ_22_Petashvili_2pk2
+{
+    enum Valences
+    {
+        I = 1,
+        II = 2,
+        III = 3,
+        IV = 4,
+        V = 5,
+    }
+
+    class Subject
+    {
+        private static int valence_1 = 0;
+        private static int valence_2 = 0;
+        private static int countFreeElectrons = 0;
+        private static Valences valences;
+
+        public int CountFreeElectrons
+        {
+            get { return countFreeElectrons; }
+            set { countFreeElectrons = value; }
+        }
+
+        public static Valences Valences
+        {
+            get { return valences; }
+            set { valences = value; }
+        }
+
+        public static void PrintInfo()
+        {
+            Console.Write($"Количество элементов с первой валентностью: {valence_1}\n" +
+                          $"Количество элементов со второй валентностью: {valence_2}\n" +
+                          $"Количество свободных электронов: {countFreeElectrons}\n");
+            Console.Write($"Valences: {Valences}");
+        }
+
+        public Subject(int countElectrons)
+        {
+            countFreeElectrons = countElectrons;
+            if (countFreeElectrons == 1) ++valence_1;
+            else if (countFreeElectrons == 2) ++valence_2;
+            valences = (Valences)countFreeElectrons;
+        }
+    }
+}
