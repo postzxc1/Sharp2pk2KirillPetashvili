07fc874834f68259e864aba9520716f96e72f0b8
postzxc1 <kpetasvili@gmail.com>
2023-05-09 14:59:35 +0500

    Добавлено практическое задание pz_./pz_6

---
+++ pz_6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PZ_6_Petashvili_2PK2
+{
+    class Program
+    {
+        private static void Main()
+        {
+            // Задание первое
+            uint i = 1; // Счетчик цикла
+            uint number = 7; // Фиксированный множитель
+            uint result = 1; // Результат
+            while (i != 11)
+            {
+                result = number * i;
+                Console.WriteLine($"Произведение {number} на {i} = {result}");
+                ++i;
+            }
+            Console.WriteLine();
+
+            // Задание второе
+            uint n = Convert.ToUInt32(Console.ReadLine()); // Количество цифр для вычисления суммы
+            uint j = 0; // Счетчик цикла
+            uint result_2 = 1; // Результат
+            while (j <= n)
+            {
+                result_2 += j;
+                ++j;
+            }
+            System.Console.WriteLine($"{result_2}");
+        }
+    }
+}
