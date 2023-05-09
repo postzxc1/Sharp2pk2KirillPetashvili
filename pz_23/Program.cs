a6a7ccee9e2fae65720f3686c143715edd141db9
postzxc1 <kpetasvili@gmail.com>
2023-05-09 14:59:38 +0500

    Добавлено практическое задание pz_./pz_23

---
+++ pz_23/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PZ_23_Petashvili_2pk2
+{
+    class RadioactiveAtom : Atom
+    {
+        private int countFreeElectrons;
+        private bool radioactivity;
+
+        public RadioactiveAtom(int countElectrons) : base(countElectrons)
+        {
+            countFreeElectrons = countElectrons;
+
+            if (countFreeElectrons >= 0) radioactivity = false;
+            else if (countFreeElectrons < 0) radioactivity = true;
+        }
+
+        public bool PeriodSemiDecay
+        {
+            get { return radioactivity; }
+            set { radioactivity = value; }
+        }
+
+        public override void PrintInfo()
+        {
+            Console.Write($"Количество электронов: {countFreeElectrons}\n" +
+                          $"Является ли элемент радиоактивным: {PeriodSemiDecay}");
+        }
+    }
+}
