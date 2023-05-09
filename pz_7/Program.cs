c7d340c4e2c809a4467cb59492bc0b8dbdfbf090
postzxc1 <kpetasvili@gmail.com>
2023-05-09 14:59:38 +0500

    Добавлено практическое задание pz_./pz_7

---
+++ pz_7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PZ_7_Petashvili_2PK2
+{
+    class Program
+    {
+        private static void Main()
+        {
+            int[] numbers = new int[20];
+            Random random = new Random();
+
+            for (int i = 0; i < numbers.Length; ++i)
+            {
+                numbers[i] = random.Next(0, 100);
+                Console.Write($"Элемент массива {numbers[i]} || ");
+                bool isDuplicate = false;
+
+                for (int j = 0; j < i; ++j)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+
+                if (!isDuplicate)
+                    Console.WriteLine($"Уникальный элемент массива: {numbers[i]}");
+            }
+        }
+    }
+}
