ff7965ae781e484f0d76bfc38ae8eebc22c5c65e
postzxc1 <kpetasvili@gmail.com>
2023-05-09 14:59:34 +0500

    Добавлено практическое задание pz_./pz_12

---
+++ pz_12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PZ_12_Petashvili_2PK2
+{
+    class Program
+    {
+        private static void Quarter(int x, int y)
+        {
+            if (x > 0 && y > 0) Console.WriteLine("1 четверть");
+            if (x < 0 && y > 0) Console.WriteLine("2 четверть");
+            if (x < 0 && y < 0) Console.WriteLine("3 четверть");
+            if (x > 0 && y < 0) Console.WriteLine("4 четверть");
+        }
+
+        private static void Main()
+        {
+            for (int i = 0; i < 3; ++i)
+            {
+                Console.WriteLine("Введите координаты точек: ");
+                int x = Convert.ToInt32(Console.ReadLine());
+                int y = Convert.ToInt32(Console.ReadLine());
+                Quarter(x, y);
+            }
+        }
+    }
+}
