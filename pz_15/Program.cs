e1bb6f361473cc6a7e28fa80a42e7cca4a51b962
postzxc1 <kpetasvili@gmail.com>
2023-05-09 14:59:32 +0500

    Добавлено практическое задание pz_./pz_15

---
+++ pz_15/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace PZ_15_Petashvili_2PK2
+{
+    class Program
+    {
+        private static void GetNumber(string line)
+        {
+            char[] str = line.ToCharArray();
+            string result = "";
+            int countNumber = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsDigit(str[i]))
+                {
+                    result += Char.ToString(str[i]);
+                    if (!Char.IsDigit(str[i + 1])) countNumber += 1;
+                }
+            }
+        }
+
+        private static void Main()
+        {
+            string fileName = @"C:\Users\razin.ad1694\source\repos\inFile.txt";
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                string line = "";
+                while (line != "stop")
+                {
+                    line = Console.ReadLine();
+                    writer.WriteLine(line);
+                }
+            }
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                Console.WriteLine($"Содержимое файла:");
+                Console.WriteLine(reader.ReadToEnd());
+            }
+
+            GetNumber("Andrey222Razin777");
+        }
+    }
+}
